0351e44a9c4ea0545451900759547b6c59764aca
youssef <aitelquadiyoussef3@gmail.com.com>
2024-05-06 18:28:44 +0100

    Data table

--- src/Apps.js
+++ src/Apps.js
@@ -1,15 +1,10 @@
 import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
-import { MantineProvider } from '@mantine/core';
-
 import { Box, Button } from '@mantine/core';
 import { IconDownload } from '@tabler/icons-react';
 import { jsPDF } from 'jspdf'; 
 import autoTable from 'jspdf-autotable'; 
 import { mkConfig, generateCsv, download } from 'export-to-csv';
 import "./App.css"
-import { ChevronDown, Filter } from 'lucide-react';
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
 const columns = [
   {
     accessorKey: 'id',
@@ -52,18 +47,7 @@ const columns = [
     header: 'Country',
     size: 220,
   },
-  {
-    accessorKey: 'action',
-    header: 'Action',
-    size: 120,
-    
-  },
 ];
-const actions=<div className="button-group flex space-x-9">
-<button className="ms-5 border border-borderLine p-2 rounded-[50%] hover:bg-[#F0F7EF]"><EditIcon className="text-[#FEA928]" /></button>
-<button className="ms-5 border border-borderLine p-2 rounded-[50%] hover:bg-[#F0F7EF]"><DeleteIcon className="text-[#F24E1E]" /></button>                
-</div>
-
 const data = [
     {
       id: 1,
@@ -72,7 +56,6 @@ const data = [
       company: 'ABC Corporation',
       city: 'New York',
       country: 'USA',
-      action:actions
     },
     {
       id: 2,
@@ -81,7 +64,6 @@ const data = [
       company: 'XYZ Corp',
       city: 'Los Angeles',
       country: 'USA',
-      action:actions
     },
     {
       id: 3,
@@ -90,21 +72,16 @@ const data = [
       company: 'Acme Ltd',
       city: 'London',
       country: 'UK',
-      action:actions
     },
     // Add more sample data as needed...
   ];
+const csvConfig = mkConfig({
+  fieldSeparator: ',',
+  decimalSeparator: '.',
+  useKeysAsHeaders: true,
+});
+const Example = () => {
   
-
-
-  const csvConfig = mkConfig({
-    fieldSeparator: ',',
-    decimalSeparator: '.',
-    useKeysAsHeaders: true,
-  });
-const App = ({}) => {
-  
-
   const handleExportRowsPdf = (rows) => {
     // Check if any rows are selected
     if (Array.isArray(rows) && rows.length > 0) {
@@ -146,8 +123,6 @@ const App = ({}) => {
     }
   };
   
-
-
   const table = useMantineReactTable({
     columns,
     data,
@@ -158,16 +133,12 @@ const App = ({}) => {
     enableDensityToggle:false,
     enableFullScreenToggle:false,
     positionGlobalFilter:"left",
-    
+    color:"#549433",
     initialState: {
-      columnPinning: {  right: ['action'] },
+      columnPinning: {  right: ['country'] },
       showGlobalFilter: true,
-      
+
     },
-    mantinePaperProps: {
-      style: { '--mrt-base-background-color': '#549433' },
-    },
-    mantineTableCellProps:{color: '#549433'},
     mantineTableHeadCellProps: {sx: {
       '& .mantine-TableHeadCell-Content': {
           color: '#549433',
@@ -175,51 +146,44 @@ const App = ({}) => {
       },
   }},mantineTableBodyCellProps: {sx: {color: '#549433', 
   textAlign: 'left'}},
-            
     renderTopToolbarCustomActions: ({ table }) => (
       <Box
-        sx={{
-          display: 'flex',
-          gap: '16px',
-          padding: '8px',
-          flexWrap: 'wrap',
-          
-        }}
-      >
-  <div class="dropdown">
-  <button class="dropbtn">Export <ChevronDown /></button>
-  <div class="dropdown-content">
-    <Button
-          className='w-[80%] mb-1 mt-1 ml-4 bg-[#5e9643]'
-          //only export selected rows
-          onClick={() => handleExportRowsPdf(table.getSelectedRowModel().rows)}
-          leftIcon={<IconDownload />}
-          variant="filled"
-        >
-          PDF
-        </Button>
-        <Button
-          className='w-[80%] mb-1 mt-1 ml-4 bg-[#5e9643]'
-          //only export selected rows
-          onClick={() => handleExportRowsExcel(table.getSelectedRowModel().rows)}
-          leftIcon={<IconDownload />}
-          variant="filled"
-        >
-          Excel
-        </Button>
-  </div>
-</div>
+      sx={{
+        display: 'flex',
+        gap: '16px',
+        padding: '8px',
+        flexWrap: 'wrap',
         
-      </Box>
-    ),
-  }
-);
-
-  return <MantineProvider theme={{ primaryColor: 'green' }}>
-            <MantineReactTable table={table} />
-          </MantineProvider>;
+      }}
+    >
+<div class="dropdown">
+<button class="dropbtn">Dropdown</button>
+<div class="dropdown-content">
+  <Button
   
+        //only export selected rows
+        onClick={() => handleExportRowsPdf(table.getSelectedRowModel().rows)}
+        leftIcon={<IconDownload />}
+        variant="filled"
+      >
+        PDF
+      </Button>
+      <Button
+        
+        //only export selected rows
+        onClick={() => handleExportRowsExcel(table.getSelectedRowModel().rows)}
+        leftIcon={<IconDownload />}
+        variant="filled"
+      >
+        Excel
+      </Button>
+</div>
+</div>
+      
+    </Box>)
+  });
+
+  return <MantineReactTable table={table} />;
 };
 
-export default App;
-//dateCulturevarietenum_de_lotnumber_batchedate_semisActual_agenombre de grainesPlantes commandéesjour_apres_semispourcentage_germinationpourcentage_totaldeviationnumero_commandeclientfournisseur
+export default Example;
